c23c1c2f42e7c22ca4c3363fabab9937f6c996c8
NamesAreAPain <jbcrocker@pacbell.net>
2017-02-08 13:26:33 -0800

    Boundaries Done

--- MultiPoint/Setup.m
+++ MultiPoint/Setup.m
@@ -1,13 +1,13 @@
-PointA = struct('mass',1,'chrg',0,'loca',[0,1],'velo', [0,0], 'forc',[0,0]);
-PointB = struct('mass',1,'chrg',0,'loca',[0,0],'velo', [0,0], 'forc',[0,0]);
+PointA = struct('mass',1,'chrg',0,'loca',[3,3],'velo', [0,0], 'forc',[0,0]);
+PointB = struct('mass',1,'chrg',0,'loca',[-3,3],'velo', [0,0], 'forc',[0,0]);
+PointC = struct('mass',1,'chrg',0,'loca',[-3,-3],'velo', [0,0], 'forc',[0,0]);
+PointD = struct('mass',1,'chrg',0,'loca',[3,-3],'velo', [0,0], 'forc',[0,0]);
+PointE = struct('mass',1,'chrg',0,'loca',[0,0],'velo', [0,0], 'forc',[0,0]);
 
-PointC = struct('mass',1,'chrg',0,'loca',[1,1],'velo', [0,0], 'forc',[0,0]);
-PointD = struct('mass',1,'chrg',0,'loca',[1,0],'velo', [0,0], 'forc',[0,0]);
 
-Network1 = struct('pnts',[PointA PointB],'loca',[0 0],'velo',[0,0],'avel',0,'mass',0,'mofi',0,'forc',[0 0]);
-Network2 = struct('pnts',[PointC PointD],'loca',[0 0],'velo',[0,0],'avel',0,'mass',0,'mofi',0,'forc',[0 0]);
+Network1 = struct('pnts',[PointA PointB PointC PointD PointE],'boun',[],'area',0,'loca',[0 0],'velo',[0,0],'avel',0,'mass',0,'mofi',0,'forc',[0 0]);
+%Network2 = struct('pnts',[PointC PointD],'boun',{},'area',0,'loca',[0 0],'velo',[0,0],'avel',0,'mass',0,'mofi',0,'forc',[0 0]);
 
-Networks = [Network1 Network2];
-
+Networks = [Network1];
 
 
--- MultiPoint/centerOfMass.m
+++ MultiPoint/centerOfMass.m
@@ -3,8 +3,8 @@
     sumY = 0;
     sumM = 0;
     for P = network.pnts
-        sumX = sumX + P.loc(1)*P.mass;
-        sumY = sumY + P.loc(2)*P.mass;
+        sumX = sumX + P.loca(1)*P.mass;
+        sumY = sumY + P.loca(2)*P.mass;
         sumM = sumM + P.mass;
     end
     cenOfMass = [sumX sumY]./sumM;
